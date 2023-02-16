b18fbab9cbbd400f44e2d9f9bcef194821067073
ewinstonliu <125412545+ewinstonliu@users.noreply.github.com>
2023-02-16 23:47:58 +1300

    Update streamlit_app.py
    format fruityvice json value into a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
+
+# Normalize json value from api response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output normalized data as a table
+streamlit.dataframe(fruityvice_normalized)
